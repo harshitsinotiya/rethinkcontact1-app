9a19aaf28739b13a968e0283adf77ff606467053
harshitsinotiya <134021867+harshitsinotiya@users.noreply.github.com>
2025-11-30 01:35:25 +0530

    Update WebConfig.java

--- src/main/java/rethink/controller/WebConfig.java
+++ src/main/java/rethink/controller/WebConfig.java
@@ -1,20 +1,32 @@
-// package rethink.controller;
+/package rethink.controller;
 
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.web.servlet.config.annotation.CorsRegistry;
-// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-// @Configuration
-// public class WebConfig implements WebMvcConfigurer{
+ @Configuration
+public class WebConfig {
 
-//   public void addCorsMapping(CorsRegistry registry) {
-// 	  registry.addMapping("/**")
-// 	  .allowedOriginPatterns("*")
-// 	  .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
-// 	  .allowedHeaders("*")
-// 	  .allowCredentials(true)
-// 	  .maxAge(3600);
-//   }
-// }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "https://indexing-3kdj.onrender.com",
+                                "http://127.0.0.1:5501",
+                                "http://localhost:5501",
+                                "https://rethinkcontact1-app.onrender.com"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
+
+
 
