c4768d0fc5651588ef11c56b66f3a3f5effb887c
harshitsinotiya <134021867+harshitsinotiya@users.noreply.github.com>
2025-11-30 01:23:28 +0530

    Update ContactController.java

--- src/main/java/rethink/controller/ContactController.java
+++ src/main/java/rethink/controller/ContactController.java
@@ -7,7 +7,12 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/contacts")
-@CrossOrigin(origins = "http://127.0.0.1:5501/index.html/index.html") // safe CORS
+@CrossOrigin(origins = {
+        "https://indexing-3kdj.onrender.com",
+        "http://127.0.0.1:5501",
+        "http://localhost:5501"
+})
+ // safe CORS
 public class ContactController {
 
     private final ContactService service;
