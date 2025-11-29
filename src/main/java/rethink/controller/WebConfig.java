6f5386bf356d3d4461056128692cd19b4c0affd7
harshitsinotiya <134021867+harshitsinotiya@users.noreply.github.com>
2025-11-30 00:47:45 +0530

    Refactor WebConfig for CORS handling
    Refactor CORS configuration to use allowedOriginPatterns and maxAge.

--- src/main/java/rethink/controller/WebConfig.java
+++ src/main/java/rethink/controller/WebConfig.java
@@ -6,20 +6,14 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer{
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("https://indexing-3kdj.onrender.com") // frontend URL
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
-    }
+  public void addCorsMapping(CorsRegistry registry) {
+	  registry.addMapping("/**")
+	  .allowedOriginPatterns("*")
+	  .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+	  .allowedHeaders("*")
+	  .allowCredentials(true)
+	  .maxAge(3600);
+  }
 }
-
