0d4018c0227cc0f9c22dcbc07db2ef8c07c4b892
harshitsinotiya <134021867+harshitsinotiya@users.noreply.github.com>
2025-11-29 18:10:15 +0530

    Add CrossOrigin annotation to ContactController

--- src/main/java/rethink/controller/ContactController.java
+++ src/main/java/rethink/controller/ContactController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 // Allow your VS Code frontend
 // Allow your VS Code frontend
+@CrossOrigin(origins = "*") 
 @RestController
 @RequestMapping("/contacts")
 public class ContactController {
