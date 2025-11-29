70816625c7cd5195ee196a9c6529edf1a150293c
harshitsinotiya <134021867+harshitsinotiya@users.noreply.github.com>
2025-11-29 23:12:48 +0530

    Update CrossOrigin annotation in ContactController

--- src/main/java/rethink/controller/ContactController.java
+++ src/main/java/rethink/controller/ContactController.java
@@ -5,13 +5,9 @@ import rethink.service.ContactService;
 import rethink.contact.entity.Contact;
 import java.util.List;
 
-// @CrossOrigin(
-//         origins = "https://timely-moxie-6ee490.netlify.app",
-//         allowedHeaders = "*",
-//         methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.OPTIONS}
-// )
 @RestController
 @RequestMapping("/contacts")
+@CrossOrigin(origins = "https://frontendrethinking-3et7.vercel.app") // Allow your frontend
 public class ContactController {
 
     private final ContactService service;
