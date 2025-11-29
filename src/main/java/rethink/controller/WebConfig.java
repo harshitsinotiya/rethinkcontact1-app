924c6be31747803aa27bb4312f479d4c854172b0
harshitsinotiya <134021867+harshitsinotiya@users.noreply.github.com>
2025-11-29 20:33:00 +0530

    Update WebConfig.java

--- src/main/java/rethink/controller/WebConfig.java
+++ src/main/java/rethink/controller/WebConfig.java
@@ -1,18 +1,19 @@
-package rethink.controller;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-@Configuration
-@EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-            .allowedOrigins("https://timely-moxie-6ee490.netlify.app") // no trailing slash
-            .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
-            .allowedHeaders("*")
-            .allowCredentials(false)
-            .maxAge(3600);
-    }
-}
+// package rethink.controller;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// @Configuration
+// @EnableWebMvc
+// public class WebConfig implements WebMvcConfigurer {
+//     @Override
+//     public void addCorsMappings(CorsRegistry registry) {
+//         registry.addMapping("/**")
+//             .allowedOrigins("https://timely-moxie-6ee490.netlify.app") // no trailing slash
+//             .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+//             .allowedHeaders("*")
+//             .allowCredentials(false)
+//             .maxAge(3600);
+//     }
+// }
+
