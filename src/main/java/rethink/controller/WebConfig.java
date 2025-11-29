b6e7c73bcd9278be6f772573feb0bbd6eaeaf75b
harshitsinotiya <134021867+harshitsinotiya@users.noreply.github.com>
2025-11-29 21:44:56 +0530

    Update WebConfig.java

--- src/main/java/rethink/controller/WebConfig.java
+++ src/main/java/rethink/controller/WebConfig.java
@@ -17,7 +17,7 @@ public class WebConfig   {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://marvelous-brioche-c67b73.netlify.app/")
+                        .allowedOrigins("https://glowing-sunburst-03f03b.netlify.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
@@ -25,6 +25,7 @@ public class WebConfig   {
         };
     }
 }
+
 
 
 
