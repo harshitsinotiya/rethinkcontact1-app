85b7c597d7ff4586e0a60824beb2bfeb153a5c7a
harshitsinotiya <134021867+harshitsinotiya@users.noreply.github.com>
2025-11-29 23:11:53 +0530

    Update WebConfig.java

--- src/main/java/rethink/controller/WebConfig.java
+++ src/main/java/rethink/controller/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://frontendrethinking-3et7.vercel.app/") // remove trailing slash
+                        .allowedOrigins("https://frontendrethinking-3et7.vercel.app") // no trailing slash
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
@@ -22,10 +22,3 @@ public class WebConfig {
         };
     }
 }
-
-
-
-
-
-
-
