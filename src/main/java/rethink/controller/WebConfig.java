a34502a67b9b16e9392c809865d8484c99a5a52b
harshitsinotiya <134021867+harshitsinotiya@users.noreply.github.com>
2025-11-30 01:24:27 +0530

    Comment out WebConfig class implementation
    Comment out the entire WebConfig class and its methods.

--- src/main/java/rethink/controller/WebConfig.java
+++ src/main/java/rethink/controller/WebConfig.java
@@ -1,19 +1,20 @@
-package rethink.controller;
+// package rethink.controller;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class WebConfig implements WebMvcConfigurer{
+// @Configuration
+// public class WebConfig implements WebMvcConfigurer{
 
-  public void addCorsMapping(CorsRegistry registry) {
-	  registry.addMapping("/**")
-	  .allowedOriginPatterns("*")
-	  .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
-	  .allowedHeaders("*")
-	  .allowCredentials(true)
-	  .maxAge(3600);
-  }
-}
+//   public void addCorsMapping(CorsRegistry registry) {
+// 	  registry.addMapping("/**")
+// 	  .allowedOriginPatterns("*")
+// 	  .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+// 	  .allowedHeaders("*")
+// 	  .allowCredentials(true)
+// 	  .maxAge(3600);
+//   }
+// }
+
