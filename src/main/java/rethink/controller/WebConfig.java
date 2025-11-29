3d1e0f91aa3a072b5102099cf88a9b3dc9bd36ee
harshitsinotiya <134021867+harshitsinotiya@users.noreply.github.com>
2025-11-29 18:39:31 +0530

    Add files via upload

---
+++ src/main/java/rethink/controller/WebConfig.java
@@ -0,0 +1,16 @@
+package rethink.controller;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("https://timely-moxie-6ee490.netlify.app")
+            .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(false)
+            .maxAge(3600);
+    }
+}
