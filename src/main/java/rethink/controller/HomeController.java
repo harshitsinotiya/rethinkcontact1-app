067ee601b135f90bdb26c6ad6c0cc6fff8a080cd
harshitsinotiya <134021867+harshitsinotiya@users.noreply.github.com>
2025-11-30 21:53:52 +0530

    Update HomeController.java

--- src/main/java/rethink/controller/HomeController.java
+++ src/main/java/rethink/controller/HomeController.java
@@ -4,11 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-public class HomeController {
-
+@RestController
+public class RootController {
     @GetMapping("/")
     public String home() {
-        return "index";
+        return "Backend Running Successfully!";
     }
 }
