d4125d7ee56d656987e887ec9e86bbc78f8b07e0
harshitsinotiya <134021867+harshitsinotiya@users.noreply.github.com>
2025-11-30 17:00:28 +0530

    Update HomeController.java

--- src/main/java/rethink/controller/HomeController.java
+++ src/main/java/rethink/controller/HomeController.java
@@ -8,6 +8,6 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "Backend is running! Visit /contacts to view contacts.";
+        return "index";
     }
 }
