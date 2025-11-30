95ddb555e72bbefb218a1999e94a07eb4a4cf7da
harshitsinotiya <134021867+harshitsinotiya@users.noreply.github.com>
2025-11-30 19:36:56 +0530

    Update WebConfig.java

--- src/main/java/rethink/controller/WebConfig.java
+++ src/main/java/rethink/controller/WebConfig.java
@@ -1,25 +1,32 @@
-// package rethink.controller;
+package rethink.controller;
 
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.web.servlet.config.annotation.CorsRegistry;
-// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-// @Configuration
-// public class WebConfig {
-//     @Bean
-//     public WebMvcConfigurer corsConfigurer() {
-//         return new WebMvcConfigurer() {
-//             @Override
-//             public void addCorsMappings(CorsRegistry registry) {
-//                 registry.addMapping("/**")
-//                         .allowedOrigins("http://localhost:8080")
-//                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//                         .allowedHeaders("*");
-//             }
-//         };
-//     }
-// }
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                .allowedOrigins("https://indexing-3kdj.onrender.com", "http://localhost:3000", "*",)
+
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+                       
+                       
+            }
+        };
+    }
+}
+
+
 
 
 
