23ac0d6fc5d9fedbca9fc6c4f270b009e07a09cd
harshitsinotiya <134021867+harshitsinotiya@users.noreply.github.com>
2025-11-30 16:54:38 +0530

    Update HomeController.java

--- src/main/java/rethink/controller/HomeController.java
+++ src/main/java/rethink/controller/HomeController.java
@@ -1,13 +1,13 @@
-// package rethink.controller;
+package rethink.controller;
 
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-// @RestController
-// public class HomeController {
+@RestController
+public class HomeController {
 
-//     @GetMapping("/")
-//     public String home() {
-//         return "Backend is running! Visit /contacts to view contacts.";
-//     }
-// }
+    @GetMapping("/")
+    public String home() {
+        return "Backend is running! Visit /contacts to view contacts.";
+    }
+}
